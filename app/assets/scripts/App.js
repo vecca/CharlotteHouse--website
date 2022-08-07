d7e6141a77d2003c92abe791be0b6e38e29ccd75
Vedran Knezevic <knezevic.vedran@gmail.com>
2022-08-07 15:32:47 +1000

    Implemented Modal popup

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,10 +2,12 @@ import "../styles/styles.css";
 import Gallery from "./modules/Gallery";
 import Navigation from "./modules/Navigation";
 import StickyNav from "./modules/StickyNav";
+import Modal from "./modules/Modal";
 
 new Gallery();
 new Navigation();
 new StickyNav();
+new Modal();
 
 //closing menu after link is clicked
 document.querySelector(".navWrap").addEventListener("click", function (e) {
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,51 @@
+class Modal {
+  constructor() {
+    this.injectHTML();
+    this.modal = document.querySelector(".modal");
+    this.closeIcon = document.querySelector(".btn--close-modal");
+    this.overlay = document.querySelector(".overlay");
+    this.btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+    this.events();
+  }
+
+  events() {
+    //console.log(this.btnsOpenModal);
+    //listen for close click
+    this.closeIcon.addEventListener("click", () => this.closeTheModal());
+    this.overlay.addEventListener("click", () => this.closeTheModal());
+    //this.btnsOpenModal.forEach((btn) => btn.addEventListener("click", this.openTheModal()));
+    this.btnsOpenModal.forEach((el) => el.addEventListener("click", (e) => this.openTheModal(e)));
+  }
+
+  openTheModal() {
+    console.log("Clicked");
+    this.modal.classList.remove("hidden");
+    this.overlay.classList.remove("hidden");
+  }
+
+  closeTheModal() {
+    this.modal.classList.add("hidden");
+    this.overlay.classList.add("hidden");
+  }
+
+  injectHTML() {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="modal hidden">
+      <button class="btn--close-modal">&times;</button>
+      <div class="modal__content">
+        <h2 class="modal__header mb-sm">
+          Secure your booking today!
+        </h2>
+        <p><a href="mailto:charlottehousebrisbane@gmail.com">charlottehousebrisbane@gmail.com</a></p>
+        <a href="tel:+44 345 678 903">+44 345 678 903</a>
+      </div>
+    </div>
+    <div class="overlay hidden"></div>
+    `
+    );
+  }
+}
+
+export default Modal;
